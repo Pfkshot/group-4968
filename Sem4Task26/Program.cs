a453f08abe7c1ce014f2274217c38f7d7df7e4c6
Pfkshot <Pfkshot@gmail.com>
2023-09-22 01:06:03 +0300

    sem4

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу которая принимает на вход число и выдает количество чисел в числе 
+*/
+
+int ReadData(string msg)
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+
+int NumDigits(int num)
+{
+      return Convert.ToInt32(Math.Log(num, 10) + 1);
+}
+
+
+int num = ReadData("Введите число");
+Console.WriteLine("количество цифр в числе = " + NumDigits(num));
