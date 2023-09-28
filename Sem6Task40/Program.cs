5826ae5283a8bff11b704bdd1f96255d363a6cc5
Pfkshot <Pfkshot@gmail.com>
2023-09-29 00:51:27 +0300

    AddSem6Task40,42

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход три числа и проверяет, может ли 
+существовать треугольник с сторонами такой длины.Теорема о неравенстве треугольника: 
+каждая сторона треугольника меньше суммы двух других сторон.*/ 
+
+double ReadData(){
+    return double.Parse(Console.ReadLine()??"0");
+}
+
+
+bool isTrianglable(double a, double b, double c)
+{
+    return !(a+b <= c || b+c <= a || a+c <= b);
+}
+
+
+Console.WriteLine(isTrianglable(ReadData(),ReadData(),ReadData()));
---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.*/ 
+
+// Вывод сообщения и запись введённых данных
+int readData(string msg)
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+
+//Переводим в двоичное
+string DecToBin(int num)
+{
+    string line ="";
+    while(num>0)
+    {
+        line = num%2+line;
+        num=num/2;
+    }
+    return line;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+      Console.WriteLine(msg);
+}
+
+int num = readData("Введите число ");
+PrintResult(DecToBin(num));
+Console.WriteLine( Convert.ToString(num, 2));
+Console.WriteLine( Convert.ToString(num, 8));
+Console.WriteLine( Convert.ToString(num, 16));
