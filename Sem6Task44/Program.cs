4052632f1204a230cbbda9c71165a78056adecc1
Pfkshot <Pfkshot@gmail.com>
2023-09-30 23:17:36 +0300

    AddSem6Task44,45

---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.*/
+
+//печатаем число
+int readData(string msg)
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+
+//Печать числа фибоначи
+void PrintFibNum(int num)
+{
+      int buf = 0;
+      int first = 0;
+      int second = 1;
+      Console.Write(first + "_" + second);
+      for (int i = 2; i < num; i++)
+      {
+            Console.Write("_" + (first + second));
+            buf = first + second;
+            first = second;
+            second = buf;
+      }
+}
+//Тело программы
+int N = readData("Введите количество чисел Фибоначчи (N): ");
+PrintFibNum(N);
---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу, которая будет создавать копию заданного массива с помощью 
+поэлементного копирования.*/
+
+int[] GenArray(int num, int lowBorder, int highBorder)
+{
+      int[] arr = new int[num];
+      for (int i = 0; i < num; i++)
+      {
+            arr[i] = new Random().Next(lowBorder, highBorder + 1);
+      }
+      return arr;
+}
+//Печать массива
+void PrintArray(int[] arr)
+{
+      Console.Write("[");
+      for (int i = 0; i < arr.Length - 1; i++)
+      {
+            Console.Write(arr[i] + ", ");
+
+      }
+      Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Создаёт копию заданного массива с помощью поэлементного копирования
+int[] CopyArray(int[] arr)
+{
+      int[] copiedArr = new int[arr.Length];
+
+      // перебор элементов 
+      for (int i = 0; i < arr.Length; i++)
+            copiedArr[i] = arr[i];
+
+      return copiedArr;
+}
+
+//тело программы
+int[] arr = GenArray(15, 0, 100);
+PrintArray(arr);
+int[] copiedArr = CopyArray(arr);
+PrintArray(copiedArr);
