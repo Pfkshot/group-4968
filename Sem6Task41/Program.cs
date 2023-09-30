878973d41e5156c271722f6f0a8dab2c79c89ae1
Pfkshot <Pfkshot@gmail.com>
2023-10-01 00:14:55 +0300

    DZ_6

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте,
+ сколько чисел больше 0 ввёл пользователь.*/
+
+//печатаем число
+int readData(string msg)
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+
+//Создаем масив
+int[] GenArray(int length)
+{
+      int[] array = new int[length];
+      for (int i = 0; i < array.Length; i++)
+      {
+            array[i] = readData("Введите " + (i + 1) + "-й элемент");
+      }
+      return array;
+}
+//Печать массива
+void PrintArray(int[] arr)
+{
+      Console.Write("[");
+      for (int i = 0; i < arr.Length - 1; i++)
+      {
+            Console.Write(arr[i] + ", ");
+
+      }
+      Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+//ищем числа больше нуля
+int CountPosNum(int[] arr)
+{
+      int count = 0;
+      for (int i = 0; i < arr.Length; i++)
+      {
+            if (arr[i] > 0)
+            {
+                  count++;
+            }
+      }
+
+      return count;
+}
+
+void PrintResult(string msg)
+{
+      Console.WriteLine(msg);
+}
+
+int length = readData("Введите количество элементов");
+int[] arr = GenArray(length);
+PrintArray(arr);
+int count = CountPosNum(arr);
+PrintResult("Количество чисел > 0 = " + count);
+
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+// Вывод сообщения и запись введённых данных
+int readData(string msg)
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+      Console.WriteLine(msg);
+}
+
+// комплексный метод
+(double x, double y) CrossPoint(double k1, double b1, double k2, double b2)
+{
+      double coorX1 = (b2 - b1) / (k1 - k2);
+      double coorY1 = (k1 * b2 - k2 * b1) / (k1 - k2);
+      return (coorX1, coorY1);
+}
+
+double K1 = readData("Input number K1: ");
+double B1 = readData("Input number B1: ");
+double K2 = readData("Input number K2: ");
+double B2 = readData("Input number B2: ");
+// Поиск координат пересечения
+(double x, double y) pointA = CrossPoint(K1, B1, K2, B2);
+
+double X1 = pointA.x;
+double Y1 = pointA.y;
+
+PrintResult($"Cross points of 2 lines: X1= {X1}; Y1= {Y1}");
+
