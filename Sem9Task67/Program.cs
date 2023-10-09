c0b5e8c911428b99889596d4a623bdc9e6fd5c5d
Pfkshot <Pfkshot@gmail.com>
2023-10-10 00:37:23 +0300

    add Task 61,62,63,65,67,69

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Программа примает на вход число N и возвращает сумму цифр в числе*/
+int ReadData(string line)
+{
+      Console.Write(line);
+      int number = int.Parse(Console.ReadLine() ?? "0");
+      return number;
+}
+
+int SumDigitRec(int num)
+{
+
+      if (num == 0)
+      {
+            return 0;
+      }
+      else
+      {
+            return num % 10 + SumDigitRec(num / 10);
+      }
+
+}
+
+int number = ReadData("Введите число: ");
+System.Console.WriteLine("Сумма всех цифр в числе " + number + "равна " + SumDigitRec(number));
---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая будет принимать 2 числа А и Ввозведите А в степень В 
+с помощью рекурсии*/
+
+int ReadData(string msg) // вводим данные
+{
+      Console.WriteLine(msg);
+      int num = int.Parse(Console.ReadLine() ?? "0");
+      return num;
+}
+// возводим в степень
+long DigitPower(int n, int p)
+{
+      if (p == 1) return n;
+      if (p == 2) return n * n;
+      if (p > 0)
+      {
+            return DigitPower(n, p / 2) * DigitPower(n, p - p / 2);
+      }
+      else return 1;
+}
+int n = ReadData("Введите основание");
+int m = ReadData("Введите степень");
+Console.WriteLine(n + " в степени " + m + " = " + DigitPower(n, m));
