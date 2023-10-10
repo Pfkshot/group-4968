48589eb1aa38378c8ec3f17c54744123df3e49b5
Pfkshot <Pfkshot@gmail.com>
2023-10-10 23:55:03 +0300

    DZ_9_task_64

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void NatDig(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NatDig(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+int count = 1;
+int num = ReadData("Введите число N ");
+PrintResult("Натуральные числа в промежутке это: ");
+NatDig(num, count);
